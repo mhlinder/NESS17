42178862e67e45b09072213e243d10f7b9754b24
M. Henry Linder <mhlinder@gmail.com>
2016-09-19 14:14:35 -0400

    restyles form elements, including removing skeleton directives

--- regform.php
+++ regform.php
@@ -1,1 +1,16 @@
-<h2>This is a test</h2>
+
+<form id="registration" name="registration" method="post" enctype="multipart/form-data" action="regsubmit.php">
+  <table>
+    <tr>
+      <td><b>Short Course:</b></td>
+      <td>
+	<select name="sc">
+	  <option selected="selected">No short course</option>
+	  <option>Short Course 1</option>
+	  <option>Short Course 2</option>
+	  <option>Short Course 3</option>
+	</select>
+      </td>
+    </tr>
+  </table>
+</form>
