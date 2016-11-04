999e5e34da6048b70233a7fa18fd98e53c84d59d
M. Henry Linder <mhlinder@gmail.com>
2016-11-04 12:29:09 -0400

    fixes html-only output

--- compile.sh
+++ compile.sh
@@ -4,3 +4,8 @@ for fn in `\ls md/` ; do
     b=${fn%.*}
     pandoc --template template.html -T "NESS 2017" --smart -o html/$b.html md/$fn ;
 done
+
+for fn in `\ls html | \grep 'reg.*.html'` ; do
+    b=${fn%.*}
+    mv html/$b.html html/$b.php
+done
